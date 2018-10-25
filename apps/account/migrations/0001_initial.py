ad05fc6c464b070b9dc41f1088c37f459bea90de
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2018-10-25 08:46:37 -0700

    wewe

--- apps/account/migrations/0001_initial.py
+++ apps/account/migrations/0001_initial.py
@@ -16,8 +16,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=255, unique=True)),
-                ('firstname', models.EmailField(max_length=255)),
-                ('lastname', models.EmailField(max_length=255)),
+                ('firstname', models.CharField(max_length=255)),
+                ('lastname', models.CharField(max_length=255)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
             options={
